9e03b9b5885be1878e29965d9ad92d0b20ae2d23
kheangmeng <kheangmengte@gmail.com>
2025-09-18 18:37:44 +0700

    temp update, delete

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -3,29 +3,29 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-    const { id } = params
-    const { todo, is_completed } = await request.json()
-  let res
-  try {
-    res = await sql`UPDATE todos SET todo = ${todo}, is_completed = ${is_completed} WHERE id = ${id};`;
-    revalidatePath('/', 'layout')
-    return Response.json({ message: 'Todo updated successfully' })
-  } catch (error) {
-    console.log('TODO UPDATE ERROR:::', error)
-    return Response.json({ message: 'Error updating todo' }, { status: 500 })
-  }
-}
+// export async function PUT(request: Request, { params }: { params: { id: string } }) {
+//     const { id } = params
+//     const { todo, is_completed } = await request.json()
+//   let res
+//   try {
+//     res = await sql`UPDATE todos SET todo = ${todo}, is_completed = ${is_completed} WHERE id = ${id};`;
+//     revalidatePath('/', 'layout')
+//     return Response.json({ message: 'Todo updated successfully' })
+//   } catch (error) {
+//     console.log('TODO UPDATE ERROR:::', error)
+//     return Response.json({ message: 'Error updating todo' }, { status: 500 })
+//   }
+// }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  const { id } = params
-  let res
-  try {
-    res = await sql`DELETE FROM todos WHERE id = ${id};`;
-    revalidatePath('/', 'layout')
-    return Response.json({ message: 'Todo deleted successfully' })
-  } catch (error) {
-    console.log('TODO DELETE ERROR:::', error)
-    return Response.json({ message: 'Error deleting todo' }, { status: 500 })
-  }
-}
+// export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+//   const { id } = params
+//   let res
+//   try {
+//     res = await sql`DELETE FROM todos WHERE id = ${id};`;
+//     revalidatePath('/', 'layout')
+//     return Response.json({ message: 'Todo deleted successfully' })
+//   } catch (error) {
+//     console.log('TODO DELETE ERROR:::', error)
+//     return Response.json({ message: 'Error deleting todo' }, { status: 500 })
+//   }
+// }
